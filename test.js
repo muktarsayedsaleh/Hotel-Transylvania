b2d44825a48f38cd241724a7df11e11b94fd5b34
Muktar Sayed Saleh <mokhtar_ss@hotmail.com>
2019-06-29 21:25:51 +0800

    TDD, test skeleton for load_config() function .. just ideas for now

---
+++ test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+describe('Helper Functions', function() {
+    describe('load_config()', function() {
+        it('should return json object if called with correct file name located on the same folder', function() {
+        });
+        it('should return fallback json object if called with wrong file name,', function() {
+        });
+        it('should return an object contains maximumGuestsPerBooking attribute', function() {
+        });
+        it('should return an object contains maximumGuestsPerBookingExcludingInfants attribute', function() {
+        });
+        it('should return an object contains maximumRoomsPerBooking attribute', function() {
+        });
+        it('should return an object contains maximumGuestsPerRoom attribute', function() {
+        });
+    });
+});
