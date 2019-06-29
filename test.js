3f3af5d9fd0384bc99550b53f0c9709501923b97
Muktar Sayed Saleh <mokhtar_ss@hotmail.com>
2019-06-29 21:36:39 +0800

    TDD, Skeleton tests for booking function

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const { load_config } = require('./helpers.js');
 
 describe('Helper Functions', function() {
-    describe('load_config()', function() {
+    describe('load_config(file_path)', function() {
         it('should return json object if called with correct file name located on the same folder', function() {
             assert.equal(typeof(load_config('config.json')), 'object');
         });
@@ -21,6 +21,20 @@ describe('Helper Functions', function() {
         });
         it('should return an object contains maximumGuestsPerRoom attribute', function() {
             assert.equal(('maximumGuestsPerRoom' in load_config('config.json')), true);
+        });
+    });
+});
+
+
+describe('Booking function', function() {
+    describe('book_minimum_rooms(adult, children, infants)', function() {
+        it('should throw an Error if one or more param is not a number', function() {
+        });
+        it('should throw an Error if total of adults and children is grater than maximum allowed', function() {
+        });
+        it('should return a list of rooms if called correctly (I am thinking of list here so we can tell not only how many rooms we need(rooms.length), but also we can tell in details how many adults, children and infants are on each room)', function() {
+        });
+        it('should return 2 (a list two rooms) as a result for the given example in the question: For 3 Adults, 4 Children and 2 infants can be fit in 2 rooms and not 3.', function() {
         });
     });
 });
